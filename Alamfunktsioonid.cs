92db366bc4b8ba686dfc8c753175bfeb4c3439e9
opilane <opilane@HP-CZC2349HTB>
2026-02-25 14:09:44 +0200

    Add project files.

---
+++ Alamfunktsioonid.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kontroltöö
+{
+    internal class Alamfunktsioonid
+    {
+        
+        public static Tuple<double, double> KytuseKalkulaator()
+        {
+            Console.WriteLine("Palun sisesta oma läbitud teepikkus (km):");
+            int teepikkus = int.Parse(Console.ReadLine());
+            Console.WriteLine("Mis on sinu kütuse liitri hind?");
+            float hind = float.Parse(Console.ReadLine());
+            teepikkus = 100;
+            double kütusekulu = hind * teepikkus;
+            double reisimaksumus = teepikkus * kütusekulu;
+            
+            return Tuple.Create(kütusekulu, reisimaksumus);
+
+        }
+        
+        public static string HindaIsikukood()
+        {
+            Console.WriteLine("Sisesta oma isikukood");
+             int isikukood = int.Parse(Console.ReadLine());
+            if (isikukood.Length < 11 )
+            {
+
+            }
+        }
+
+
+
+
+
+
+    }
+}
---
+++ StartPage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kontroltöö
+{
+    internal class StartPage
+    {
+        public static void Main(string[] args)
+        {
+            bool tööta = true;
+
+            while (tööta)
+            {
+                Console.WriteLine("Vali tegevus:\r\n1 - Kütuse kalkulaator\r\n2 - Isikukoodi analüüs\r\n3 - Täringumäng\r\n4 - Palgaarvestus\r\n0 - Välju\r\n");
+                string valik = Console.ReadLine();
+                switch (valik)
+                {
+                    case "1":
+                        var tulemus = Alamfunktsioonid.KytuseKalkulaator();
+                        
+                        Console.WriteLine($"kui palju kütust: {tulemus.Item1:F2}");
+                        Console.WriteLine($" reisi maksumus: {tulemus.Item2:F2}");
+                        break;
+                    
+                    case "2":
+
+                    break;
+                    
+                    case "3":
+
+                    break;
+                    
+                    case "4":
+
+                    break;
+                    case "0":
+                        tööta = false;
+                        Console.WriteLine("Nägemist!");
+                        break;
+
+                }
+
+            }
+        }
+    }
+}
